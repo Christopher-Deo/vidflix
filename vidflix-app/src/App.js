716b03b909edbf18fc701b37cfc6587a5ea0d1c1
Christopher Deo <110053896+Christopher-Deo@users.noreply.github.com>
2022-11-11 09:14:47 -0600

    Start of react app

---
+++ vidflix-app/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/header/Header';
+import './App.css';
+import '@fontsource/roboto/300.css';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />  
+      <div className="App">
+      <Footer />
+      </div>
+    </>
+  );
+}
+export default App;
---
+++ vidflix-app/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+import SimpleBottomNavigation from "../botton-nav/Bottom-nav"; 
+
+function Footer() {
+    return (
+        <div className="footer">
+        <SimpleBottomNavigation />
+        </div>
+    );
+    }
+    export default Footer;
